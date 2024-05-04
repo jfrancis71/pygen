ffb716f3d8b5be681866c11e21278c145fd15c55
Julian Francis <julian.w.francis@gmail.com>
2024-05-04 10:34:15 +0100

    Adding dummy_run parameter

--- train/train.py
+++ train/train.py
@@ -59,9 +59,9 @@ class _Trainer():
 
 class DistributionTrainer(_Trainer):
     def __init__(self, trainable, dataset, batch_size=32, max_epoch=10, batch_end_callback=None,
-                 epoch_end_callback=None, use_scheduler=False):
+                 epoch_end_callback=None, use_scheduler=False, dummy_run=False):
         super(DistributionTrainer, self).__init__(trainable, dataset, batch_size, max_epoch, batch_end_callback,
-                                           epoch_end_callback, use_scheduler=use_scheduler)
+                                           epoch_end_callback, use_scheduler=use_scheduler, dummy_run=dummy_run)
 
     def log_prob(self, batch):
         return self.trainable.log_prob(batch[0].to(self.device))
@@ -69,9 +69,9 @@ class DistributionTrainer(_Trainer):
 
 class LayerTrainer(_Trainer):
     def __init__(self, trainable, dataset, batch_size=32, max_epoch=10, batch_end_callback=None,
-                 epoch_end_callback=None, use_scheduler=False):
+                 epoch_end_callback=None, use_scheduler=False, dummy_run=False):
         super(LayerTrainer, self).__init__(trainable, dataset, batch_size, max_epoch, batch_end_callback,
-                 epoch_end_callback, use_scheduler=use_scheduler)
+                 epoch_end_callback, use_scheduler=use_scheduler, dummy_run=dummy_run)
 
     def log_prob(self, batch):
         return self.trainable(batch[0].to(self.device)).log_prob(batch[1].to(self.device))
