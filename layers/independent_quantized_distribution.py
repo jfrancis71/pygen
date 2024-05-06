254f1ca5b5bea4b5c7f7d1de171e0b9223a97a82
Julian Francis <julian.w.francis@gmail.com>
2024-05-06 09:00:51 +0100

    Bugfix to batch_shape of IndependentQuantizedDistribution

--- layers/independent_quantized_distribution.py
+++ layers/independent_quantized_distribution.py
@@ -13,7 +13,7 @@ class IndependentQuantizedDistribution(torch.nn.Module):
         return math.prod(self.event_shape)*self.num_buckets
 
     def forward(self, logits):  # logits, eg. B, Y, X, P where batch_shape would be B, Y, X
-        batch_shape = list(logits.shape[:-len(self.event_shape)])
+        batch_shape = list(logits.shape[:-1])
         reshape_logits = logits.reshape(batch_shape + self.event_shape + [self.num_buckets])
         base_distribution = qd.QuantizedDistribution(logits=reshape_logits)
         return torch.distributions.independent.Independent(base_distribution, reinterpreted_batch_ndims=len(self.event_shape))
