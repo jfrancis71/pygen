9357f5806be00a7a3f0a93cba53bc3b006512d8c
Julian Francis <julian.w.francis@gmail.com>
2024-05-22 16:05:45 +0300

    Pylintifying

--- layers/categorical.py
+++ layers/categorical.py
@@ -1,9 +1,18 @@
+"""
+Defines Categorical layer.
+"""
+
+
 import torch
 
 
 class Categorical(torch.nn.Module):
+    """Categorical is a layer which accepts a tensor as input and returns a probability
+    distribution. Suitable for use directly in, eg Sequential, presumably as final layer.
+    """
     def __init__(self):
         super().__init__()
 
+    # pylint: disable=C0116
     def forward(self, logits):
         return torch.distributions.categorical.Categorical(logits=logits)
