029f8aa902e29f1a479d7cf344a1a398d83c8a71
Julian Francis <julian.w.francis@gmail.com>
2024-08-18 07:18:19 +0100

    Adding option as to whether to sample with noise in quantized distribution.

--- distributions/quantized_distribution.py
+++ distributions/quantized_distribution.py
@@ -8,6 +8,10 @@ from torch.distributions.categorical import Categorical
 import torch
 
 
+def discretize(value, num_buckets):
+    return torch.clamp((value * num_buckets).floor(), 0, num_buckets - 1) / num_buckets + 1.0 / (num_buckets * 2)
+
+
 class QuantizedDistribution:
     """A continuous distribution on interval (0,1) which has been discretized into num_buckets.
 
@@ -24,12 +28,13 @@ class QuantizedDistribution:
         >>> qd.sample([3]).shape
         torch.Size([3, 7])
     """
-    def __init__(self, logits):
+    def __init__(self, logits, add_noise):
         self.logits = logits
         self.num_buckets = self.logits.shape[-1]
         self.event_shape = torch.Size([])  # pylint: disable=E1101
         self.batch_shape = self.logits.shape[:-1]
         self.log_buckets = torch.log(torch.tensor(self.num_buckets))  # pylint: disable=E1101
+        self.add_noise = add_noise
 
     def log_prob(self, value):
         """returns log_prob of value under this distribution."""
@@ -40,7 +45,10 @@ class QuantizedDistribution:
     def sample(self, sample_shape=torch.Size()):  # pylint: disable=E1101
         """sample from this distribution."""
         floor = Categorical(logits=self.logits).sample(sample_shape)/self.num_buckets
-        samples = floor + torch.rand_like(floor)/(self.num_buckets*2.0)  # pylint: disable=E1101
+        if self.add_noise:
+            samples = floor + torch.rand_like(floor)/self.num_buckets  # pylint: disable=E1101
+        else:
+            samples = floor + 1.0/(self.num_buckets*2)
         return samples
 
 
--- layers/independent_quantized_distribution.py
+++ layers/independent_quantized_distribution.py
@@ -23,10 +23,11 @@ class IndependentQuantizedDistribution(torch.nn.Module):
         >>> independent_qd_distribution.sample([2]).shape
         torch.Size([2, 7, 3, 32, 32])
     """
-    def __init__(self, event_shape, num_buckets=8):
+    def __init__(self, event_shape, num_buckets=8, add_noise=True):
         super().__init__()
         self.event_shape = event_shape
         self.num_buckets = num_buckets
+        self.add_noise = add_noise
 
     def params_size(self):
         """return number of parameters required to describe distribution"""
@@ -36,7 +37,7 @@ class IndependentQuantizedDistribution(torch.nn.Module):
     def forward(self, logits):  # logits, e.g. B, Y, X, P where batch_shape would be B, Y, X
         batch_shape = list(logits.shape[:-1])
         reshape_logits = logits.reshape(batch_shape + self.event_shape + [self.num_buckets])
-        base_distribution = qd.QuantizedDistribution(logits=reshape_logits)
+        base_distribution = qd.QuantizedDistribution(logits=reshape_logits, add_noise=self.add_noise)
         return torch.distributions.independent.Independent(
             base_distribution,
             reinterpreted_batch_ndims=len(self.event_shape))
