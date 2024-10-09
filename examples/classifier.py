e94ba2ae0b5ea2945133bf474cd5557b83c84fa4
Julian Francis <julian.w.francis@gmail.com>
2024-10-09 15:56:30 +0100

    Remove class labels (they're already in the dataset).

--- examples/classifier.py
+++ examples/classifier.py
@@ -29,12 +29,10 @@ transform = transforms.Compose([transforms.ToTensor(), train.DevicePlacement()])
 if ns.dataset == "mnist":
     dataset = datasets.MNIST(ns.datasets_folder, train=True, download=False, transform=transform)
     mnist = True
-    class_labels = [f"{num}" for num in range(10)]
     data_split = [55000, 5000]
 elif ns.dataset == "cifar10":
     dataset = datasets.CIFAR10(ns.datasets_folder, train=True, download=False, transform=transform)
     mnist = False
-    class_labels = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
     data_split = [45000, 5000]
 else:
     raise RuntimeError(f"{ns.dataset} not recognized.")
@@ -49,18 +47,18 @@ classifier = torch.nn.Sequential(
     layer_categorical.IndependentCategorical(event_shape=[], num_classes=10))
 epoch_end_callbacks = [
     callbacks.tb_log_image(tb_writer, "train_images",
-        callbacks.demo_classify_images(classifier, example_train_images, class_labels)),
+        callbacks.demo_classify_images(classifier, example_train_images, dataset.classes)),
     callbacks.tb_log_image(tb_writer, "valid_images",
-                           callbacks.demo_classify_images(classifier, example_valid_images, class_labels)),
+                           callbacks.demo_classify_images(classifier, example_valid_images, dataset.classes)),
     callbacks.tb_epoch_log_metrics(tb_writer),
     callbacks.tb_dataset_metrics_logging(tb_writer, "validation", validation_dataset)]
 if ns.images_folder is not None:
     epoch_end_callbacks.append(
         callbacks.file_log_image(ns.images_folder,"train",
-            callbacks.demo_classify_images(classifier, example_train_images, class_labels)))
+            callbacks.demo_classify_images(classifier, example_train_images, dataset.classes)))
     epoch_end_callbacks.append(
         callbacks.file_log_image(ns.images_folder,"valid",
-            callbacks.demo_classify_images(classifier, example_valid_images, class_labels)))
+            callbacks.demo_classify_images(classifier, example_valid_images, dataset.classes)))
 train.train(classifier, train_dataset, train.layer_objective(track_accuracy=True),
     batch_end_callback=callbacks.tb_batch_log_metrics(tb_writer),
     epoch_end_callback=callbacks.callback_compose(epoch_end_callbacks), dummy_run=ns.dummy_run)
