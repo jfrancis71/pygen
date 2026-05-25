955be11416f89ef0a6575820d03f196be9f8a91d
Julian Francis <julian.w.francis@gmail.com>
2026-05-25 07:17:51 +0000

    Reordering...
    Grouping epoch callbacks together.

--- ignite/mnist.py
+++ ignite/mnist.py
@@ -1,10 +1,8 @@
 # This example is from https://github.com/pytorch/ignite/tree/master
 # See above for license.
 
-from argparse import ArgumentParser
-
+import argparse
 import torch
-import torch.nn.functional as F
 from torch import nn
 from torch.optim import SGD
 from torch.utils.data import DataLoader
@@ -12,23 +10,23 @@ from torch.utils.tensorboard import SummaryWriter
 from torch.utils.data import random_split
 import torchvision.datasets as datasets
 from torchvision.transforms import Compose, Normalize, ToTensor
-
 from ignite.engine import create_supervised_evaluator, create_supervised_trainer, Events
 from ignite.metrics import Accuracy, Loss
 from ignite.utils import setup_logger
 from train import callbacks
-import layers.independent_categorical as layer_categorical
 from neural_nets import classifier_net
+import layers.independent_categorical as layer_categorical
 
 
-parser = ArgumentParser()
+parser = argparse.ArgumentParser()
 parser.add_argument("--datasets_folder", default="~/datasets")
-parser.add_argument("--max_epoch", type=int, default=10, help="number of epochs to train (default: 10)")
 parser.add_argument("--tb_folder", default=None)
 parser.add_argument("--device", default="cpu")
+parser.add_argument("--max_epoch", type=int, default=10, help="number of epochs to train (default: 10)")
 args = parser.parse_args()
-data_transform = Compose([ToTensor(), Normalize((0.1307,), (0.3081,))])
-dataset = datasets.MNIST(args.datasets_folder, train=True, download=True, transform=data_transform)
+
+transform = Compose([ToTensor(), Normalize((0.1307,), (0.3081,))])
+dataset = datasets.MNIST(args.datasets_folder, train=True, download=True, transform=transform)
 data_split = [55000, 5000]
 train_dataset, validation_dataset = random_split(dataset, data_split,
     generator=torch.Generator(device=torch.get_default_device()))
@@ -44,33 +42,23 @@ trainer.logger = setup_logger("trainer")
 val_metrics = {"accuracy": Accuracy(), "nll": Loss(criterion)}
 evaluator = create_supervised_evaluator(model, metrics=val_metrics, device=args.device)
 evaluator.logger = setup_logger("evaluator")
-
 example_valid_images = next(iter(torch.utils.data.DataLoader(validation_dataset, batch_size=25)))[0]
-
 tb_writer = SummaryWriter(args.tb_folder)
-
 classifier = torch.nn.Sequential(
     model,
     layer_categorical.IndependentCategorical(event_shape=[], num_classes=10)
 )
 
 @trainer.on(Events.EPOCH_COMPLETED)
-def log_training_results(engine):
+def log_results(engine):
     evaluator.run(train_loader)
     metrics = evaluator.state.metrics
-    avg_accuracy = metrics["accuracy"]
-    avg_nll = metrics["nll"]
-    tb_writer.add_scalar("training/avg_accuracy", avg_accuracy, engine.state.epoch)
-    tb_writer.add_scalar("training/avg_loss", avg_nll, engine.state.epoch)
-
-@trainer.on(Events.EPOCH_COMPLETED)
-def log_validation_results(engine):
+    tb_writer.add_scalar("training/avg_accuracy", metrics["accuracy"], engine.state.epoch)
+    tb_writer.add_scalar("training/avg_loss", metrics["nll"], engine.state.epoch)
     evaluator.run(val_loader)
     metrics = evaluator.state.metrics
-    avg_accuracy = metrics["accuracy"]
-    avg_nll = metrics["nll"]
-    tb_writer.add_scalar("validation/avg_accuracy", avg_accuracy, engine.state.epoch)
-    tb_writer.add_scalar("valiation/avg_loss", avg_nll, engine.state.epoch)
+    tb_writer.add_scalar("validation/avg_accuracy", metrics["accuracy"], engine.state.epoch)
+    tb_writer.add_scalar("validation/avg_loss", metrics["nll"], engine.state.epoch)
     image = callbacks.demo_classify_images(classifier, example_valid_images, dataset.classes)()
     if tb_writer is not None:
         tb_writer.add_image("valid_images", image, engine.state.epoch)
