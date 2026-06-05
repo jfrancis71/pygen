6944e1af923969ad6344b1aaf1bf5f27ad5e6c69
Julian Francis <julian.w.francis@gmail.com>
2026-06-05 15:33:31 +0000

    Refactoring...

--- ignite/mnist.py
+++ ignite/mnist.py
@@ -5,15 +5,15 @@ import argparse
 import torch
 from torch.optim import Adam
 from torch.utils.data import DataLoader, random_split
-from torch.utils.tensorboard import SummaryWriter
 from torchvision.datasets import MNIST
 from torchvision.transforms import ToTensor
 from ignite.engine import create_supervised_evaluator, create_supervised_trainer, Events
 from ignite.metrics import Accuracy, Loss
 from ignite.utils import setup_logger
+from ignite.handlers.utils import global_step_from_engine
+from ignite.handlers.tensorboard_logger import TensorboardLogger, OutputHandler
 from pygen.train import callbacks
 from pygen.neural_nets import classifier_net
-import pygen.layers.independent_categorical as layer_categorical
 
 
 parser = argparse.ArgumentParser()
@@ -36,31 +36,26 @@ optimizer = Adam(model.parameters(), lr=.001)
 criterion = torch.nn.CrossEntropyLoss()
 trainer = create_supervised_trainer(model, optimizer, criterion, device=args.device)
 trainer.logger = setup_logger("trainer")
-val_metrics = {"accuracy": Accuracy(), "nll": Loss(criterion)}
-evaluator = create_supervised_evaluator(model, metrics=val_metrics, device=args.device)
-evaluator.logger = setup_logger("evaluator")
+metrics = {"accuracy": Accuracy(), "nll": Loss(criterion)}
+evaluator = create_supervised_evaluator(model, metrics=metrics, device=args.device)
+evaluator.logger = setup_logger("Evaluator")
 example_valid_images = next(iter(torch.utils.data.DataLoader(validation_dataset,
     batch_size=25)))[0].to(args.device)
-tb_writer = SummaryWriter(args.tb_folder)
+tb_logger = TensorboardLogger(args.tb_folder)
 
 @trainer.on(Events.EPOCH_COMPLETED)
 def log_results(engine):
-    evaluator.run(train_loader)
-    metrics = evaluator.state.metrics
-    tb_writer.add_scalar("training/avg_accuracy", metrics["accuracy"],
-        engine.state.epoch)
-    tb_writer.add_scalar("training/avg_loss", metrics["nll"], engine.state.epoch)
-    evaluator.run(val_loader)
-    metrics = evaluator.state.metrics
-    tb_writer.add_scalar("validation/avg_accuracy", metrics["accuracy"],
-        engine.state.epoch)
-    tb_writer.add_scalar("validation/avg_loss", metrics["nll"], engine.state.epoch)
+    for tag, loader in [("train", train_loader), ("validation", val_loader)]:
+        evaluator.run(loader)
+        output_handler = OutputHandler(tag, list(metrics.keys()),
+            global_step_transform=global_step_from_engine(trainer))(
+                evaluator, tb_logger, Events.EPOCH_COMPLETED)
     trainer_state = Warning() # bit of a hack here.
     trainer_state.epoch_num = engine.state.epoch
     callbacks.log_image_cb(
         callbacks.demo_classify_images(
             model, example_valid_images, dataset.classes),
-        tb_writer=tb_writer, folder=args.images_folder, name="valid_images")(
+        tb_writer=tb_logger.writer, folder=args.images_folder, name="valid_images")(
             trainer_state)
 
 trainer.run(train_loader, max_epochs=args.max_epoch)
